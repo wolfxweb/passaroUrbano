98ffc95503634240ffb0de3d1c52bf185d88a162
Eduardo Wolf <wolfxweb@gmail.com>
2021-06-21 13:05:43 -0300

    Craido o service e modelo Ofertas e altrado template home listar os produtos dinamicamente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { OfertasService } from '../sevices/ofertas.Services';//inportação do serviço ofertas
+
+import { Oferta } from '../model/oferta.model';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers:[OfertasService]// adicionado o serviço via provaider e declado o mesmo no construtor
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public produtos: Oferta[] =[]
+  constructor( private ofertasService: OfertasService  ) { }
 
   ngOnInit(): void {
+    this.produtos = this.ofertasService.getOfertas()// atribui as ofertas a uma variavel local para ser utilziado no template
+
+
+    console.log(this.produtos[0].imagens[0].urlImg)
   }
 
 }
